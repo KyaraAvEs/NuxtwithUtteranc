64e5f44e2f9834b2edabf15be12d03ebfb05b227
Kyara0797 <kyara.avalose@gmail.com>
2024-10-26 17:10:17 -0600

    commit sin nuxt

---
+++ components/global/FooterView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="row"  style="margin-top: 15px">
+    <button disabled="disabled" class="twelve columns" style="background-color: darkmagenta; color: white;">
+      Copyright (c) 2024 -Kyara Ávalos-Carlos López 
+    </button>
+  </div>
+</template>
---
+++ readme.md
@@ -0,0 +1,3 @@
+Link al proyecto: https://proyecto1carloslopezkyaraavalo.netlify.app/
+
+Integrantes: Carlos López y Kyara Avalo
